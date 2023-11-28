f03caf468ef6b1ed3d325f9c45df9b829ad2ae45
unclejames2610 <udealaebuka@gmail.com>
2023-11-28 17:28:42 +0100

    fix:nav and hero chnages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -63,28 +63,30 @@ const Navbar = () => {
           )}
         </div>
       </div>
-      <div className="bg-slate-400">
-        <div className="lg:flex gap-2 items-center text-mainPurple hidden">
-          <span className="font-mulish font-bold">Categories</span>
-          <IoMdArrowDropdown />
-        </div>
+      {navOpen && (
+        <div className="bg-white gap-8 flex flex-col p-4">
+          <div className="flex justify-between items-center text-mainPurple">
+            <span className="font-mulish font-bold">Categories</span>
+            <IoMdArrowDropdown />
+          </div>
 
-        <div className="lg:flex items-center gap-2 rounded-xl bg-[#D9D9D9] p-2 hidden">
-          <span className="text-mainPurple">
-            <AiOutlineSearch />
-          </span>
-          <input
-            className="text-mainPurple bg-transparent placeholder:text-mainPurple/30 text-sm leading-[18px] font-mulish font-bold focus:outline-none"
-            placeholder="Search for courses"
-          />
+          <div className="flex items-center gap-2 rounded-xl bg-[#D9D9D9] p-2 ">
+            <span className="text-mainPurple">
+              <AiOutlineSearch />
+            </span>
+            <input
+              className="text-mainPurple bg-transparent placeholder:text-mainPurple/30 text-sm leading-[18px] font-mulish font-bold focus:outline-none"
+              placeholder="Search for courses"
+            />
+          </div>
+          <button className="text-mainPurple px-6 py-2 rounded-lg bg-white font-mulish font-[800] text-[15px] leading-[22.5px] border-2 border-black ">
+            Sign In
+          </button>
+          <button className="text-white px-4 py-2 rounded-lg bg-mainPurple font-mulish font-[800] text-[15px] leading-[22.5px] border-2 border-mainPurple ">
+            Join Kuda
+          </button>
         </div>
-        <button className="text-mainPurple px-6 py-2 rounded-lg bg-white font-mulish font-[800] text-[15px] leading-[22.5px] border-2 border-black hidden lg:block">
-          Sign In
-        </button>
-        <button className="text-white px-4 py-2 rounded-lg bg-mainPurple font-mulish font-[800] text-[15px] leading-[22.5px] border-2 border-mainPurple hidden lg:block ">
-          Join Kuda
-        </button>
-      </div>
+      )}
     </div>
   );
 };
